26834ab519c2b27377402e75bd3ead7f0cc4761c
CrowPD <anubis.dead.god@gmail.com>
2024-05-10 22:27:47 +0300

    Changed equals() and hashCode() functions for the Book entity

--- src/main/java/guru/springframework/sdjpaintro/domain/Book.java
+++ src/main/java/guru/springframework/sdjpaintro/domain/Book.java
@@ -26,6 +26,20 @@ public class Book {
         this.publisher = publisher;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
     public Long getId() {
         return id;
     }
