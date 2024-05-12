f10d33b9ef9cdf4517bbe6926148c3348721eba1
CrowPD <anubis.dead.god@gmail.com>
2024-05-12 22:37:34 +0300

    Added test execution order and Spring Boot-specific transaction commit between tests

--- src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSlice.java
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSlice.java
@@ -2,24 +2,47 @@ package guru.springframework.sdjpaintro;
 
 import guru.springframework.sdjpaintro.domain.Book;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 public class SpringBootJpaTestSlice {
     @Autowired
     BookRepository bookRepository;
 
+    /**
+     * Spring runs tests inside a transaction.
+     * After the test, transaction is rolled back.
+     * {@code @Commit} or {@code @Rollback(false)} prevents the rollback and data can be used later
+     */
     @Test
+    @Order(1)
+    @Commit
     void testJpaTestSlice() {
         long countBefore = bookRepository.count();
+
+        assertThat(countBefore).isEqualTo(0);
+
         bookRepository.save(new Book("My Book", "4566", "Self Publish"));
 
         long countAfter = bookRepository.count();
 
         assertThat(countBefore).isLessThan(countAfter);
+    }
+
+    @Test
+    @Order(2)
+    void testJpaTestSliceTransaction() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(1);
     }
 }
