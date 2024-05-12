91cea81daf6478dd8203bf7b8ddacfb44eb4a51b
CrowPD <anubis.dead.god@gmail.com>
2024-05-12 23:00:12 +0300

    Added bootstrapping to tests via component scan

--- src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSlice.java
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSlice.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.annotation.Rollback;
 
@@ -15,6 +16,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
+@ComponentScan(basePackages = {
+        "guru.springframework.sdjpaintro.bootstrap"
+})
 public class SpringBootJpaTestSlice {
     @Autowired
     BookRepository bookRepository;
@@ -30,7 +34,7 @@ public class SpringBootJpaTestSlice {
     void testJpaTestSlice() {
         long countBefore = bookRepository.count();
 
-        assertThat(countBefore).isEqualTo(0);
+        assertThat(countBefore).isEqualTo(2);
 
         bookRepository.save(new Book("My Book", "4566", "Self Publish"));
 
@@ -43,6 +47,6 @@ public class SpringBootJpaTestSlice {
     @Order(2)
     void testJpaTestSliceTransaction() {
         long countBefore = bookRepository.count();
-        assertThat(countBefore).isEqualTo(1);
+        assertThat(countBefore).isEqualTo(3);
     }
 }
