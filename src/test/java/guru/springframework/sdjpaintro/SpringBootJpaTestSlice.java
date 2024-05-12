73386e612575a3aa479c1cb3a96943c5fb8c100a
CrowPD <anubis.dead.god@gmail.com>
2024-05-12 22:24:47 +0300

    Added JPA-specific test, that only prepares JPA-related objects

---
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSlice.java
@@ -0,0 +1,25 @@
+package guru.springframework.sdjpaintro;
+
+import guru.springframework.sdjpaintro.domain.Book;
+import guru.springframework.sdjpaintro.repositories.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class SpringBootJpaTestSlice {
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testJpaTestSlice() {
+        long countBefore = bookRepository.count();
+        bookRepository.save(new Book("My Book", "4566", "Self Publish"));
+
+        long countAfter = bookRepository.count();
+
+        assertThat(countBefore).isLessThan(countAfter);
+    }
+}
