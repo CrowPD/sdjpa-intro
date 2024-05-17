44dda50337af3afd9833d4ff0816b30406b93463
CrowPD <anubis.dead.god@gmail.com>
2024-05-18 00:20:05 +0300

    Added base configuration to run H2 DB in MySQL compatibility mode

--- src/test/java/guru/springframework/sdjpaintro/SdjpaIntroApplicationTests.java
+++ src/test/java/guru/springframework/sdjpaintro/SdjpaIntroApplicationTests.java
@@ -3,11 +3,13 @@ package guru.springframework.sdjpaintro;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class SdjpaIntroApplicationTests {
 
     @Autowired
