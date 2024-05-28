a49e6e925a0bd01ed2d74ca5d5192c6aa860d5b1
CrowPD <anubis.dead.god@gmail.com>
2024-05-28 21:04:27 +0300

    Added Author entity

---
+++ src/main/resources/db/migration/V2__add_author.sql
@@ -0,0 +1,17 @@
+drop table if exists author;
+create table author
+(
+    id         bigint not null,
+    first_name varchar(255),
+    last_name  varchar(255),
+    primary key (id)
+) engine = InnoDB;
+
+drop table if exists author_seq;
+create table author_seq
+(
+    next_val bigint
+) engine = InnoDB;
+
+insert into book_seq
+values (1);
